c95992b607df3a831d92e77a06c42dd0590c0ccd
Tanish Gupta <tanishgupta12389@gmail.com>
2024-12-17 18:54:48 +0530

    adding files

--- streamlit_HBD.py
+++ streamlit_HBD.py
@@ -1,18 +1,29 @@
 import streamlit as st
+
 # title 
 st.header("Happy Birthday! 🎂" )
+
 #can show on the left or right side of the page
 st.write("Happy birthday to you ... ")
+
 #color picker box 
 color=st.color_picker("Pic a color ")
+
 # number slider
 number=st.slider("Pick a no. ", 0,100)
+
 # print the data
 # print.write(f'you select {color} and {number}')
+
 # add a button 
 if st.button("Click Me!"):
     st.write("Hello There ! How are you ? ")
 else :
     st.write("GoodBye !")
 
+# adding a radio button 
+genre=st.radio(
+    "what is your movie genera : "
+    ("Drama", "Action","Entertainment","Thriller")
+)
 st.balloons()
