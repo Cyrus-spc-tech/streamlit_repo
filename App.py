d9b22ea63d486ceee57e469e5beae37d2421bdca
Tanish Gupta <tanishgupta12389@gmail.com>
2024-12-18 18:36:43 +0530

    Adding Data Vizulizer Web

--- App.py
+++ App.py
@@ -7,3 +7,9 @@ st.title("Data Vizulizer")
 uplode_file=st.file_uploader("Choose a Csv file", type="csv")
 if uplode_file is not None:
     st.write("File Uploded ... ")
+
+
+df=pd.read_csv(uplode_file)
+
+st.subheader("Data Preview : ")
+st.write(df.head())
