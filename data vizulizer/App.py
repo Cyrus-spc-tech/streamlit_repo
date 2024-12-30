b6d07658b0076a3389a000ba55c7893f6c3b90c5
Tanishgupta <156533347+Cyrus-spc-tech@users.noreply.github.com>
2024-12-30 20:41:23 +0530

    Add files via upload

---
+++ data vizulizer/App.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("_ Data Vizulizer _")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Data Preview")
+    st.write(df.head())# it will show the first five enteries 
+
+    st.subheader("Data Summary")
+    st.write(df.describe())# it will describe the  row and columns of the data
+
+    st.subheader("Filter Data")
+    filter = st.selectbox("Select a column to filter by", df.columns.tolist())# use to select the columns 
+    
+    unique_val=df[filter].unique()
+    selected_val=st.selectbox("select value",unique_val)
+
+    filtered_data=df[df[filter]==selected_val]
+    st.write(filtered_data)
+
+    st.subheader("  plotting data ")
+    xaxis=st.selectbox(" Select x- axis ",df.columns)
+    yaxis=st.selectbox(" Select y- axis ",df.columns)
+    if st.button("Generate Plot"):
+         st.line_chart(filtered_data.set_index(xaxis)[yaxis])
+    else:
+        st.write("Please click the button to generate the plot")
