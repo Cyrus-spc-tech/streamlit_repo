d628c6ad8005b5970fea5f544c551ab54b3f603e
Tanish Gupta <tanishgupta12389@gmail.com>
2025-02-12 22:58:11 +0530

    adding streamlit revise

---
+++ all.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("This is the title ")
+st.subheader("This is the subheader")
+
+
+
+
+st.text("This is the text")
+st.write("This is the write")
+st.markdown("This is the markdown")
+st.markdown("[Sportify](https://open.spotify.com/)  [Youtube](https://www.youtube.com/)")
+st.caption("This is the caption ")
+
+st.markdown("___")
+
+st.latex(" \\pm\\sqrt{a^2 + b^2}") #kathex go to the site and lern more math symbols
+
+st.markdown("___")
+
+
+st.success("This is the success")
+st.info("This is the info")
+st.warning("This is the warning")
