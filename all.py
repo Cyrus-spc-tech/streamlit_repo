b3861f4367550541b95efdda79c6b7ec3283a930
Tanish Gupta <tanishgupta12389@gmail.com>
2025-02-14 23:02:40 +0530

    Updating

--- all.py
+++ all.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import time
 st.title("This is the title ")
 st.subheader("This is the subheader")
 
@@ -22,3 +22,23 @@ st.markdown("___")
 st.success("This is the success")
 st.info("This is the info")
 st.warning("This is **the** warning")
+
+st.error("This is the error")
+st.exception("This is the exception")
+
+st.markdown("___")   
+col1, col2 = st.columns(2)
+col1.title("This is Column1")
+col1.subheader("This is the subheading")
+col1.code("import sklearn as sk\n"
+          "import pandas as pd")
+
+col2.title("This is Column2")
+col2.subheader("This is the subheading")
+with st.spinner("""Loading..."""):
+    time.sleep(5)
+    st.balloons()
+if st.button("Snow"):
+    with st.spinner("Loading..."):
+        time.sleep(5)
+        st.snow()
