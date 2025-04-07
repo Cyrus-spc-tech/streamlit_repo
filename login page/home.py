c7ce58bf1e046db46770c0fb84bcfcd91e92a08c
Tanish Gupta <tanishgupta12389@gmail.com>
2025-04-07 23:54:27 +0530

    Adding file

--- login page/home.py
+++ login page/home.py
@@ -1,12 +1,54 @@
 import streamlit as st
+import smtplib
+import random 
+from email.message import EmailMessage
 
-# Check for query parameters to display different content
-query_params = st.query_params
+st.set_page_config(page_title="Home", page_icon="🏠" )
+st.markdown("<h1 style='text-align:center; color:lightgreen; '>OTP TESTER</h1>",unsafe_allow_html=True)
+st.markdown("<h2 style='text-align:center; color:lightgreen;'>Login Here</h2>",unsafe_allow_html=True)
 
-# Using key notation
-if query_params.get("page") == "home":
-    st.title("Home Page")
-    st.write("Welcome to the home page!")
-else:
-    st.title("Access Denied")
-    st.write("You need to log in first.")
+otp=""
+
+for i in range(6):
+    otp+=str(random.randint(0,9))
+
+
+server=smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+
+server.login("tanishgupta12389@gmail.com",'rfbb itxr nbwp dwut')
+
+
+name=st.text_input("Enter Your name :")
+phone=st.number_input("Enter Your Phone Number :",placeholder="+91 XXXXXXXXXX",step=1)
+email=st.text_input("Enter Your Email :")
+
+if st.button("Send OTP"):
+    msg=EmailMessage()
+    msg['Subject']="OTP verify"
+    msg['from']="cyrus"
+    msg['to']=email
+    msg.set_content("Your OTP is: "+otp)
+    server.send_message(msg)
+    print('mail sent')
+    print(otp)
+    server.quit()
+
+    st.success("OTP sent to your email address")
+
+st.text("Enter the OTP sent to your email address")
+otp_input=st.text_input("Enter the OTP :")
+if st.button("Verify OTP"):
+    if otp_input==otp:
+        st.success("OTP verified successfully")
+    else:
+        st.error("invalid OTP")
+st.text("If you have not received the OTP yet, please check your spam folder or try again later.")
+
+
+
+
+
+
+    
+
