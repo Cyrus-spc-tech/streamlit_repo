8ccef852435bf46e4360b1f1493fd587b32a1d18
Tanish Gupta <tanishgupta12389@gmail.com>
2025-04-08 19:31:54 +0530

    Adding Session state

--- login page/home.py
+++ login page/home.py
@@ -1,54 +1,45 @@
 import streamlit as st
 import smtplib
-import random 
+import random
 from email.message import EmailMessage
 
-st.set_page_config(page_title="Home", page_icon="🏠" )
-st.markdown("<h1 style='text-align:center; color:lightgreen; '>OTP TESTER</h1>",unsafe_allow_html=True)
-st.markdown("<h2 style='text-align:center; color:lightgreen;'>Login Here</h2>",unsafe_allow_html=True)
-
-otp=""
-
-for i in range(6):
-    otp+=str(random.randint(0,9))
+st.set_page_config(page_title="Home", page_icon="🏠")
+st.markdown("<h1 style='text-align:center; color:lightgreen;'>OTP TESTER</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align:center; color:lightgreen;'>Login Here</h2>", unsafe_allow_html=True)
 
 
-server=smtplib.SMTP("smtp.gmail.com",587)
+if 'otp' not in st.session_state:
+    st.session_state.otp = ""
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 
-server.login("tanishgupta12389@gmail.com",'rfbb itxr nbwp dwut')
+server.login("tanishgupta12389@gmail.com", 'rfbb itxr nbwp dwut')
 
-
-name=st.text_input("Enter Your name :")
-phone=st.number_input("Enter Your Phone Number :",placeholder="+91 XXXXXXXXXX",step=1)
-email=st.text_input("Enter Your Email :")
+name = st.text_input("Enter Your name :")
+phone = st.number_input("Enter Your Phone Number :", placeholder="+91 XXXXXXXXXX", step=1)
+email = st.text_input("Enter Your Email :")
 
 if st.button("Send OTP"):
-    msg=EmailMessage()
-    msg['Subject']="OTP verify"
-    msg['from']="cyrus"
-    msg['to']=email
-    msg.set_content("Your OTP is: "+otp)
+
+    st.session_state.otp = "".join([str(random.randint(0, 9)) for _ in range(6)])
+    msg = EmailMessage()
+    msg['Subject'] = "OTP verify"
+    msg['from'] = "cyrus"
+    msg['to'] = email
+    msg.set_content("Your OTP is: " + st.session_state.otp)
     server.send_message(msg)
     print('mail sent')
-    print(otp)
+    print(st.session_state.otp)
     server.quit()
 
     st.success("OTP sent to your email address")
 
 st.text("Enter the OTP sent to your email address")
-otp_input=st.text_input("Enter the OTP :")
+otp_input = st.text_input("Enter the OTP :")
 if st.button("Verify OTP"):
-    if otp_input==otp:
+    if otp_input == st.session_state.otp:
         st.success("OTP verified successfully")
     else:
-        st.error("invalid OTP")
+        st.error("Invalid OTP")
 st.text("If you have not received the OTP yet, please check your spam folder or try again later.")
-
-
-
-
-
-
-    
-
