a4b463ffda3c370cdcac3eeeb7555fe9dbddb020
Tanish Gupta <tanishgupta12389@gmail.com>
2025-04-23 21:43:22 +0530

    Adding

--- data vizulizer/App.py
+++ data vizulizer/App.py
@@ -1,13 +1,22 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+st.set_page_config(page_title="Data Vizulizer", page_icon=":bar_chart:", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 
 st.title("_ Data Vizulizer _")
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
+st.sidebar.title("Data Vizulizer")
+
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    try:
+     df = pd.read_csv(uploaded_file)
+    except Exception as e:
+        st.error(f"Error reading the file: {e}")
+        st.stop()
+    st.success("File uploaded successfully!")
 
     st.subheader("Data Preview")
     st.write(df.head())# it will show the first five enteries 
@@ -27,7 +36,24 @@ if uploaded_file is not None:
     st.subheader("  plotting data ")
     xaxis=st.selectbox(" Select x- axis ",df.columns)
     yaxis=st.selectbox(" Select y- axis ",df.columns)
+
+    if "plot_option" not in st.session_state:
+        st.session_state.plot_option = None
+
     if st.button("Generate Plot"):
-         st.line_chart(filtered_data.set_index(xaxis)[yaxis])
-    else:
+        st.session_state.plot_option = st.sidebar.radio("Select a Graph you want to plot ", ("Line Graph", "Bar Graph", "Pie Chart"))
+
+    if st.session_state.plot_option:
+        if st.session_state.plot_option == "Line Graph":
+            st.subheader("Line Graph")
+            st.line_chart(filtered_data.set_index(xaxis)[yaxis])
+        elif st.session_state.plot_option == "Bar Graph":
+            st.subheader("Bar Graph")
+            st.bar_chart(filtered_data.set_index(xaxis)[yaxis])
+        elif st.session_state.plot_option == "Pie Chart":
+            st.subheader("Pie Chart")
+            plt.figure(figsize=(10, 6))
+            plt.pie(filtered_data[yaxis], labels=filtered_data[xaxis], autopct='%1.1f%%')
+            plt.title(f"Pie Chart of {yaxis} by {xaxis}")
+            st.pyplot(plt)
         st.write("Please click the button to generate the plot")
