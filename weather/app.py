9d6ebaaa1d9465de3a11332597ba4fb00b1f19b3
Tanish Gupta <tanishgupta12389@gmail.com>
2025-05-12 17:47:20 +0530

    Adding new fieldesa

--- weather/app.py
+++ weather/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-
+import pandas as pd
 # Function to fetch weather data
 def get_weather_data(location):
     api_key = "c978c0732a3d3e0b91f6a62b22c7cc27"
@@ -11,14 +11,27 @@ def get_weather_data(location):
 # Streamlit app layout
 st.title("Weather Dashboard 🌤️")
 location = st.text_input("Enter a city name:")
+col1, col2 = st.columns(2)
 
 if location:
     weather_data = get_weather_data(location)
     if weather_data.get("main"):
-        st.subheader(f"Current Weather in {location}")
-        st.write(f" 🌡️ Temperature: {weather_data['main']['temp']} °C")
-        st.write(f" ☁️ Weather: {weather_data['weather'][0]['description']}")
-        st.write(f" 💧 Humidity: {weather_data['main']['humidity']}%")
-        st.write(f" 💨 Wind Speed: {weather_data['wind']['speed']} m/s ")
+        
+        
+        with col1:
+            st.write(f" 🗺️ Current Weather in {location}")
+           
+            st.write(f" 🌡️ Temperature: {weather_data['main']['temp']} °C")
+            st.write(f" ☁️ Weather: {weather_data['weather'][0]['description']}")
+            st.write(f" 💧 Humidity: {weather_data['main']['humidity']}%")
+            st.write(f" 💨 Wind Speed: {weather_data['wind']['speed']} m/s ")
+            st.write(f" 🌍 Coordinates: {weather_data['coord']['lon']}, {weather_data['coord']['lat']}")
+        with col2:
+            st.write(f" 🌞 Sunrise: {pd.to_datetime(weather_data['sys']['sunrise'], unit='s')}")
+            st.write(f" 🌜 Sunset: {pd.to_datetime(weather_data['sys']['sunset'], unit='s')}")
+            st.write(f" 🪟 Visibility: {weather_data['visibility'] / 1000} km")
+            st.write(f" 🌤️ Cloudiness: {weather_data['clouds']['all']}%")
+            st.write(f" 📍 Pressure: {weather_data['main']['pressure']} hPa")
+            st.write(f" ☁️ Weather Icon: ![Icon](http://openweathermap.org/img/wn/{weather_data['weather'][0]['icon']}@2x.png)")
     else:
         st.error("Location not found. Please try again.")
