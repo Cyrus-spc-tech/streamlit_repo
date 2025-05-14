5930eed185b535454aa59be41a928e7754be6750
Tanish Gupta <tanishgupta12389@gmail.com>
2025-05-14 18:56:57 +0530

    adding code line

--- weather/app.py
+++ weather/app.py
@@ -12,7 +12,7 @@ def get_weather_data(location):
 # Streamlit app layout
 st.title("Weather Dashboard 🌤️")
 location = st.text_input("Enter a city name:")
-col1, col2 = st.columns(2)
+col1, col2= st.columns(2)
 
 if location:
     weather_data = get_weather_data(location)
@@ -20,19 +20,23 @@ if location:
         
         
         with col1:
-            st.write(f" 🗺️ Current Weather in {location}")
+            st.code(f" 🗺️ Current Weather in {location}")
            
-            st.write(f" 🌡️ Temperature: {weather_data['main']['temp']} °C")
-            st.write(f" ☁️ Weather: {weather_data['weather'][0]['description']}")
-            st.write(f" 💧 Humidity: {weather_data['main']['humidity']}%")
-            st.write(f" 💨 Wind Speed: {weather_data['wind']['speed']} m/s ")
-            st.write(f" 🌍 Coordinates: {weather_data['coord']['lon']}, {weather_data['coord']['lat']}")
+            st.code(f" 🌡️ Temperature: {weather_data['main']['temp']} °C")
+            st.code(f" ☁️ Weather: {weather_data['weather'][0]['description']}")
+            st.code(f" 💧 Humidity: {weather_data['main']['humidity']}%")
+            st.code(f" 💨 Wind Speed: {weather_data['wind']['speed']} m/s ")
+            st.code(f" 🌍 Coordinates: {weather_data['coord']['lon']}, {weather_data['coord']['lat']}")
+            # st.code(f" 🌡️ Min Temperature: {weather_data['main']['temp_min']} °C")
+            # st.code(f" 🌡️ Max Temperature: {weather_data['main']['temp_max']} °C")
+
         with col2:
-            st.write(f" 🌞 Sunrise: {pd.to_datetime(weather_data['sys']['sunrise'], unit='s')}")
-            st.write(f" 🌜 Sunset: {pd.to_datetime(weather_data['sys']['sunset'], unit='s')}")
-            st.write(f" 🪟 Visibility: {weather_data['visibility'] / 1000} km")
-            st.write(f" 🌤️ Cloudiness: {weather_data['clouds']['all']}%")
-            st.write(f" 📍 Pressure: {weather_data['main']['pressure']} hPa")
+            st.code(f" 🌞 Sunrise: {pd.to_datetime(weather_data['sys']['sunrise'], unit='s')}")
+            st.code(f" 🌜 Sunset: {pd.to_datetime(weather_data['sys']['sunset'], unit='s')}")
+            st.code(f" 🪟 Visibility: {weather_data['visibility'] / 1000} km")
+            st.code(f" 🌤️ Cloudiness: {weather_data['clouds']['all']}%")
+            st.code(f" 📍 Pressure: {weather_data['main']['pressure']} hPa")
             st.write(f" ☁️ Weather Icon: ![Icon](http://openweathermap.org/img/wn/{weather_data['weather'][0]['icon']}@2x.png)")
+
     else:
         st.error("Location not found. Please try again.")
