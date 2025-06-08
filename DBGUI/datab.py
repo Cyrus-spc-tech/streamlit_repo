4bb681d66975deb1217faac24719954099af50b5
Tanish Gupta <tanishgupta12389@gmail.com>
2025-06-08 16:13:21 +0530

    Adding insert , fetch function

--- DBGUI/datab.py
+++ DBGUI/datab.py
@@ -3,3 +3,24 @@ import sqlite3
 con = sqlite3.connect("Users.db")
 cur=con.cursor()
 
+cur.execute("""CREATE TABLE IF NOT EXIST user(id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+email TEXT NOT NULL,
+password TEXT NOT NULL)""")
+con.commit()
+con.close()
+
+def insert(name, email, password):
+    con = sqlite3.connect("Users.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO user (name, email, password) VALUES (?, ?, ?)", (name, email, password))
+    con.commit()
+    con.close()
+
+def fetch():
+    con = sqlite3.connect("Users.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM user")
+    data = cur.fetchall()
+    con.close()
+    return data
