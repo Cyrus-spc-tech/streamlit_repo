e40d065e4ed46968e68843984b97f0c43aad9949
Tanish Gupta <tanishgupta12389@gmail.com>
2025-07-08 11:31:10 +0530

    fixes

--- ytdownloader/app.py
+++ ytdownloader/app.py
@@ -7,7 +7,7 @@ import re
 
 # Set page configuration
 st.set_page_config(
-    page_title="YouTube Video Downloader",
+    page_title="YT Downloader",
     page_icon="📥",
     layout="centered",
     initial_sidebar_state="auto"
